8fa5a3036d0f14929e6ab125a272ccbc4eb0edd6
andrycodestuffs <andrybusa@gmail.com>
2019-02-01 20:32:13 +0100

    Fixed #87

--- libs/layout.js
+++ libs/layout.js
@@ -1,7 +1,11 @@
-import { Dimensions } from 'react-native'
+import { Dimensions, Platform } from 'react-native'
+import ExtraDimensions from 'react-native-extra-dimensions-android'
 
 module.exports = {
   get visibleHeight() {
-    return Dimensions.get('screen').height
+    return Platform.select({
+      android: ExtraDimensions.get('REAL_WINDOW_HEIGHT') - ExtraDimensions.get('SOFT_MENU_BAR_HEIGHT'),
+      default: Dimensions.get('window').height
+    })
   },
 }
